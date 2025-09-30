d7fc39f0a98b6fa282ce9a557514cd68ec2b3541
Mike Kenway <mike@mikekenway.com>
2025-09-30 19:36:49 -0400

    tcp listener works

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -8,6 +8,7 @@ import (
 	"net"
 )
 
+// Reads lines from a stream and emits them into a channel
 func getLinesFromReader(f io.ReadCloser) <-chan string {
 	out := make(chan string, 1)
 
@@ -15,51 +16,50 @@ func getLinesFromReader(f io.ReadCloser) <-chan string {
 		defer f.Close()
 		defer close(out)
 
-	str := ""
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
+		str := ""
+		for {
+			data := make([]byte, 8) // read in 8-byte chunks
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
 
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			out <- str
+		}
 	}()
+
 	return out
 }
 
 func main() {
 	listener, err := net.Listen("tcp", ":42069")
 	if err != nil {
-		log.Fatal("error", "error", err)
+		log.Fatal("error", err)
 	}
+	defer listener.Close()
 
-	for connection, err := listener.Accept()
-		if err != nil {
-			log.Fatal("error", "error", err)
-	}
-
-	for line := range getLinesFromReader(connection) {
-		fmt.Printf("read: %s\n", line)
-	}
-
-	str := ""
 	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
+		conn, err := listener.Accept()
 		if err != nil {
-			break
+			log.Fatal("accept error:", err)
 		}
 
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
-			data = data[i+1:]
-			out <- str
-			str = ""
-		}
-		str += string(data)
-	}
-
-	if leng(str) != 0 {
-		fmt.Printf("read: %s\n", str)
+		go func(c net.Conn) {
+			defer c.Close()
+			for line := range getLinesFromReader(c) {
+				fmt.Printf("read: %s\n", line)
+			}
+		}(conn)
 	}
 }
